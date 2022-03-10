6707dba74f87a60287c01e721ebb81f5593b0ab6
Rodri24 <eltoti10@hotmail.com>
2022-03-10 17:00:17 -0300

    agregue ScrollReveal y modifique las animaciones de nav menu

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
-scrollReveal().reveal('.principal');
-scrollReveal().reveal('.servicios');
-scrollReveal().reveal('.logos-marca');
+
+ScrollReveal().reveal('.principal');
+ScrollReveal().reveal('.servicios', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 500 });
+ScrollReveal().reveal('.opciones', { delay: 500 });
+ScrollReveal().reveal('.logos-marca', { delay: 500 });
+ScrollReveal().reveal('footer', { delay: 500 });
